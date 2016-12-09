84dc066fa850e8437fda3cffb199aa1cb05e7abf
Felix Vandelanotte & Lijie Yang & mark tanca & Nadav Appel <pair+felixvdl.jessieyang0320.mtanca.nadavp3@devbootcamp.com>
2016-12-09 17:09:24 -0500

    working MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,21 +4,3 @@ get '/' do
 end
 
 
-get '/create' do
-  erb :'/decks/create'
-end
-
-post '/create' do
-@new_desk = params[:title]
-d = Deck.new(name: params[:title])
-d.save
-redirect '/'
-end
-
-
-get '/decks/:id' do
-  @decks = Deck.all
-  @get_cards = Flashcard.all
-
-erb :'decks/show'
-end
